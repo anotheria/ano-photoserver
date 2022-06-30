f5d8fa30262af9cc4d7cf2c6cb2124008d34dced
HELLnyk <yurock.ua@gmail.com>
2022-06-30 13:51:14 +0300

    fixed NPE

--- java/net/anotheria/anosite/photoserver/api/photo/fs/PhotoStorageToFoldersFSService.java
+++ java/net/anotheria/anosite/photoserver/api/photo/fs/PhotoStorageToFoldersFSService.java
@@ -186,6 +186,7 @@ public class PhotoStorageToFoldersFSService implements CrudService<PhotoFileHold
         try {
             PhotoFileHolder photoFileHolder = new PhotoFileHolder(PhotoStorageUtil.getId(id.getOwnerId()), PhotoStorageUtil.getOriginalId(id.getOwnerId()), PhotoStorageUtil.getExtension(id.getOwnerId()));
             photoFileHolder.setPhotoFileInputStream(new FileInputStream(file));
+            photoFileHolder.setFileLocation(file.getPath().substring(0, file.getPath().lastIndexOf(File.separator)));
             return photoFileHolder;
         } catch (FileNotFoundException e) {
             return null;
